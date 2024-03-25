7e587d8332f432a0e447a5b2db40e583fc017e7d
masud45827 <masud.cse2019@gmail.com>
2024-03-25 16:35:30 +0600

    folder add

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
+app.get('/',(req,res)=>{
+    res.send('succesfullysd')
+})
 app.listen(8000, () => {
     console.log('8000 page listen')
 })
-//masudsfd
