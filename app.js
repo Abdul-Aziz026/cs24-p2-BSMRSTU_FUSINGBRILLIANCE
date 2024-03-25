cc507bb0de5e21e58aede4eb3a81d053fcec2ade
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-03-25 16:21:12 +0600

    seond

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express();
+
+app.get("/home", (req, res)=>{
+    res.send("home page");
+})
+
 app.listen(8000, () => {
     console.log('8000 page listen')
 })
