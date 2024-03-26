36714e1c36f1176abd73d484a9f961c67799392d
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-03-26 10:48:15 +0600

    mongo connection setup

--- databaseConCheck.js
+++ databaseConCheck.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 
 // mongo connection check
 const MONGO_URL = process.env.MONGO_URL;
-// const MONGO_URL = "mongodb+srv://alhamdulillah026:Alhamdulillah18CSE026@clustaer0.xvjeezh.mongodb.net/Hackathon-p2";
 
 main()
 .then(() => console.log("connection Successfull in database."))
