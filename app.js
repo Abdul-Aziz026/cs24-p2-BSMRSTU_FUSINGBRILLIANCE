172ec849c6d839f7e1494d3cb57d8af2aff92dfb
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-03-26 16:41:50 +0600

    commit fuad

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
 
-<<<<<<< HEAD
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const mongoose = require("mongoose");
-const stsRoute = require('./routes/stsRouter.js');
-require("./databaseConCheck.js");
-
-const app = express();
-
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Define route for handling POST requests to /sts endpoint
-app.use('/sts', stsRoute);
-
-// Start the server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
-=======
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
@@ -51,4 +30,3 @@ app.listen(8000, () => {
     console.log('8000 page listen')
 })
 //aziz_branch...
->>>>>>> 6bd435a687b37b6fc71c3809e69e5881b665aeb4
