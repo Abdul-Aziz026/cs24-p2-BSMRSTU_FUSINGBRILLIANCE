5e4248ea41ffdab53f2b2ec45a9e0eb0da92d2f5
Fuad7161 <fuadul202@gmail.com>
2024-03-26 11:00:31 +0600

    Fuadul

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
+console.log('hi');
+app.get('/',(req,res)=>{
+    res.send("HI")
+})
 app.listen(8000, () => {
     console.log('8000 page listen')
 })
