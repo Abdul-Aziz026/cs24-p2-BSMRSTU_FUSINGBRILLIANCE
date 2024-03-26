c6b3e86c46e3012824b9a852e90fa5ad69aa0ac9
Fuad7161 <fuadul202@gmail.com>
2024-03-26 11:09:18 +0600

    fuadul

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express();
+require("./databaseConCheck.js");
+
+app.get("/home", (req, res)=>{
+    res.send("home page");
+})
+app.listen(8000, () => {
+    console.log('8000 page listen')
+})
+//aziz_branch...
