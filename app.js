4164bf57b8842044963f8bf9fb2e4306fe43aa59
Fuad7161 <fuadul202@gmail.com>
2024-03-28 15:22:46 +0600

    home added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/home", (req, res)=>{
-    res.render("login.ejs");
+    res.render("dashboard.ejs");
 });
 
 // const mailsend= require('./routes/emailJsRoute.js');
@@ -42,6 +42,8 @@ app.use('/waste_collection', wasteCollectionRoute);
 // Landfill transfar route....
 const LandfillTransferRoute = require('./routes/LandfillTransferRouter.js');
 app.use('/landfill_transfer', LandfillTransferRoute);
+
+
 
 app.listen(8000, () => {
     console.log('8000 page listen')
